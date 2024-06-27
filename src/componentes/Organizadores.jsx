a4d09183d1e7449a6efd050c9671ef28acbcbdc8
arielmarno <arielmarno@outlook.com>
2024-06-27 12:07:18 -0300

    feat(assets: folders(plantas and organizadores), components: links(plantas and organizadores), index: favicon, navbar: responsive)

--- src/componentes/Organizadores.jsx
+++ src/componentes/Organizadores.jsx
@@ -1,55 +1,62 @@
 import '../estilos/img.css';
-import ph from '../assets/placeholder-image.png';
+
+import uno from '../assets/organizadores/h3.webp';
+import dos from '../assets/organizadores/c3a.webp';
+import tres from '../assets/organizadores/c3b.webp';
+import cuatro from '../assets/organizadores/v3.webp';
+import cinco from '../assets/organizadores/c3c.webp';
+import seis from '../assets/organizadores/c3d.webp';
+import siete from '../assets/organizadores/c3e.webp';
 const Organizadores = () => {
   return (
     <div className='contenedor_img'>
       <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={uno} alt="organizador" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={dos} alt="organizador" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={tres} alt="organizador" />
+          </figure>
+        </a>
+        </div>
+        <div className='box_img'>
+        <a href="https://www.muebles44.com/vestidores/vestidor-con-estantes-y-perchero-1-m-vs001">
+          <figure>
+            <img src={cuatro} alt="organizador" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={cinco} alt="organizador" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={seis} alt="organizador" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
-        </a>
-        </div>
-        <div className='box_img'>
-        <a href="">
-          <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={siete} alt="organizador" />
+          </figure>
         </a>
         </div>
     </div>
--- src/componentes/PlantasImg.jsx
+++ src/componentes/PlantasImg.jsx
@@ -1,5 +1,12 @@
 import '../estilos/img.css';
-import ph from '../assets/placeholder-image.png';
+
+import uno from '../assets/plantas/h1.webp';
+import dos from '../assets/plantas/c1a.webp';
+import tres from '../assets/plantas/c1b.webp';
+import cuatro from '../assets/plantas/v1.webp';
+import cinco from '../assets/plantas/c1c.webp';
+import seis from '../assets/plantas/c1d.webp';
+import siete from '../assets/plantas/c1e.webp';
 
 const PlantasImg = () => {
   return (
@@ -7,50 +14,50 @@ const PlantasImg = () => {
       <div className='box_img'>
         <a href="">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={uno} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa033-sansieveira">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={dos} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa029-topiarios">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={tres} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa023-palmera-areca-190">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={cuatro} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa016-topiario-con-flores">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={cinco} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa017-cactus-grande">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={seis} alt="plantas" />
+          </figure>
         </a>
         </div>
         <div className='box_img'>
-        <a href="">
+        <a href="https://www.muebles44.com/plantas-artificiales/pa032-orquideas">
           <figure>
-              <img src={ph} alt="" />
-            </figure>
+            <img src={siete} alt="plantas" />
+          </figure>
         </a>
         </div>
     </div>
